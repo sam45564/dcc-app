a153335c712dede8862623a981c051ed71c4c3a4
Samsul Hoque Choudhury <sam45564@gmail.com>
2021-01-05 20:02:36 +0530

    Removed default collection file and added files in server and api based on the requirement.

--- server/main.js
+++ server/main.js
@@ -1,31 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { UserCollection } from '/imports/api/users';
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
+const insertUser = ({ name, count }) => UserCollection.insert({ name, count });
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
+  if (UserCollection.find().count() === 0) {
+    [
+      { name: "A", count: 0 },
+      { name: "B", count: 0 },
+    ].forEach(insertUser);
   }
 });
